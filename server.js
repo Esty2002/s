8f1ff3c5fe2b72aab09d3cb19a276a12206523e9
תכנות <@690156GUR.LOCAL>
2023-03-03 10:07:12 +0200

    create app,package ,server

---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+const http=require('http')
+const {connect} =require('./dal/db/mongo/mongo-connections')
+
+const {app}=require('./app')
+
+const {HOST,PORT}=process.env
+
+connect().then(_=>{
+    console.log('mongo is connected');
+
+    app.listen(PORT,HOST,()=>{
+        console.log(`http://${HOST}:${PORT}`);
+    })
+})
+
+const server=http.createServer(app)
