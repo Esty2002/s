64ae676def27e5bf746a468ee9654e67d8c1cad1
תכנות <@690156GUR.LOCAL>
2023-03-12 15:30:54 +0200

    insert tashtiot lelo shtuyot

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+const create_router = require('./routers/create');
+const read_router = require('./routers/read');
+const update_router = require('./routers/update');
+const delete_router = require('./routers/delete');
+
+app.use('/create', create_router);
+app.use('/read', read_router);
+app.use('/update', update_router);
+app.use('/delete', delete_router);
+
+module.exports = { app };
