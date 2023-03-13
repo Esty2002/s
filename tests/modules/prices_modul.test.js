293535a0b5db4bb0bc56398f85c023645288b764
תכנות <@690156GUR.LOCAL>
2023-03-13 19:22:53 +0200

    add folder for the tests

---
+++ tests/modules/prices_modul.test.js
@@ -0,0 +1,24 @@
+const { getConnection, connect, disconnect } = require("../../services/sql/sql-connection");
+
+
+describe('sql operation',()=>{
+    let myconfig = {  
+        server: 'TB6-19\\MSSQLSERVER',  //update me 
+        port:1433,
+        user: 'project', //update me
+        password: '1234',  //update me
+        database: 'Byton',
+        options: {
+            
+          
+           trustServerCertificate: true
+              //update me
+        }
+    }; 
+
+ 
+
+
+
+
+})
