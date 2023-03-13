ef77d29ac78c6ced65ab706edd32f09fadcf0339
תכנות <@690156GUR.LOCAL>
2023-03-13 19:38:28 +0200

    git tovi from kontext 13/13/23

--- modules/suppliers.js
+++ modules/suppliers.js
@@ -1,4 +1,5 @@
 const {getAll,insert}=require('../db/sql-operation');
+const {SQL_DB_SUPPLIERS } = process.env;
 
 async function getallsuppliers(){
     const result = await getAll('Suppliers')
@@ -8,7 +9,7 @@ async function insertsuppliers(Obj){
     console.log("-----------",Obj);
     console.log(Object.keys(Obj).join(",")+"Object.keys(Obj).join(",")");
     console.log(Object.values(Obj)+"Object.values(Obj)");
-    const result = await insert("Suppliers",Object.keys(Obj).join(","),Object.values(Obj).join(","))
+    const result = await insert(SQL_DB_SUPPLIERS,Object.keys(Obj).join(","),Object.values(Obj).join(","))
     return result;
 }
 
