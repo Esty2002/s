d95e652aa771f094aa779e5d514dd0b8e47e236a
תכנות <@690156GUR.LOCAL>
2023-03-14 15:41:59 +0200

    improve delete 14/03/2023

--- modules/suppliers.js
+++ modules/suppliers.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { getAll, insert, getByValues, del ,changeDisabele,setDate,changeDisabledDate} = require('../db/sql-operation');
+const { getAll, insert, getByValues, del ,insertSupplier,changeDisabele,setDate,changeDisabledDate} = require('../db/sql-operation');
 const {SQL_DB_SUPPLIERS } = process.env;
 
 // פונקציה ששולחת לפונקציות מחיקה
@@ -7,10 +7,9 @@ async function deletesupplier(object) {
     const date=await setDate()
     const newDate=date.recordset[0].Today
     console.log(newDate);
-    //////////////////////////להוסיף משתנה גלובלי
-    const resultSupplierCode = await del(SQL_DB_SUPPLIERS, '36', Object.values(object)[0])
-    const resultSupplierCode2 = await changeDisabele(SQL_DB_SUPPLIERS, '36')
-    const resultSupplierCode3 = await changeDisabledDate(SQL_DB_SUPPLIERS, '36', newDate)
+    console.log(Object.values(object)[0]);
+    //////////////////////////לקבל משתנה גלובלי
+    const resultSupplierCode = await del(SQL_DB_SUPPLIERS, '11', Object.values(object)[0],newDate)
     return resultSupplierCode
 
 }
@@ -21,10 +20,12 @@ async function getallsuppliers(){
 }
 
 async function insertsuppliers(Obj){
-    console.log("-----------",Obj);
     console.log(Object.keys(Obj).join(",")+"Object.keys(Obj).join(",")");
     console.log(Object.values(Obj)+"Object.values(Obj)");
-    const result = await insert(SQL_DB_SUPPLIERS,Object.keys(Obj).join(","),Object.values(Obj).join(","))
+    // const result = await insert(SQL_DB_SUPPLIERS,Object.keys(Obj).join(","),Object.values(Obj).join(","))
+       const result = await insertSupplier(Obj)
+
+    
     return result;
 }
 
