93e3e6964ebb3c1adddf9dbdf58703773febce12
תכנות <@690156GUR.LOCAL>
2023-03-14 18:53:54 +0200

    before push the changes to dev and merge

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express=require('express')
-
 const app=express()
+const cors = require('cors')
 
+const createClient_router=require('./routers/createClient')
+app.use(cors())
+app.use('/createClient', createClient_router)
 
+app.get('/', async (req, res) => {
+    console.log('Hello');
+})
 
 module.exports={app}
