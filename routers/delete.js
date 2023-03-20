98d165dd6f09290a36ac97ec01c82012da35ff79
תכנות <@690156GUR.LOCAL>
2023-03-20 18:22:16 +0200

    as

--- routers/delete.js
+++ routers/delete.js
@@ -4,9 +4,7 @@ const { findItem, updateProduct } = require('../modules/products')
 
 
 router.get('/find', async (req, res) => {
-    console.log("stannnnnnnnnn");
     const name = req.query.name
-    console.log("nameeeeeeeeeeee",name);
     const project = { traitName: name }
     const result = await findItem(project, { _id: 0, traitName: 1, must: 1, ordinalNumber: 1, must: 1,
          shortTrait: 1, values: 1, addedDate: 1, enabled: 1 })
--- server.js
+++ server.js
@@ -19,13 +19,11 @@ const r=new MongoDBOperations()
 const a=r.insertOne({ordinalNumber:1,traitName:'ss',must:true,shortTrait:'1'
 ,values:[{name:'stam',addedDate:"28/03/03"}],addedDate:"12/03/23",enabled:true})
    
-console.log("aaaaaaaaaaaa",a);
     
     
 
 sconnect().then(_=>{
     console.log('connect to sql');
-    console.log("stam");
 })
 
 const server = http.createServer(app)
