13fb64b3948ec992ab737bf0501ada171711d92c
תכנות <@690156GUR.LOCAL>
2023-03-21 19:24:52 +0200

    second after

--- routers/manageQuotation.js
+++ routers/manageQuotation.js
@@ -1,11 +1,37 @@
-const express = require('express');
+const express = require('express')
 const router = express.Router();
 
-const { DeleteQuat } = require('../modules/quotation/delete')
+const { Insert, Delete, Update ,commentAccordingCode,updateAccordingCode} = require('../modules/quotation/update')
+const {  DeleteQuat } = require('../modules/quotation/delete')
 
 
 router.get('/', (req, res) => {
-    res.send({ message: 'Welcome to router' })
+    res.send({ message: 'welcome to router' })
+})
+
+router.get('/commentAccordingCode/:code', async (req, res) => {
+    const result = await commentAccordingCode(req.params.code);
+    res.send( result )
+})
+
+router.get('/updateAccordingCode/:code/:price', async (req, res) => {
+    const result = await updateAccordingCode(req.params);
+    res.send( result )
+})
+
+router.post('/insertQuotationItems', express.json(), async (req, res) => {
+    const result = await Insert(req.body);
+    res.send({ result })
+})
+
+router.post('/deleteQuotationItems', express.json(), async (req, res) => {
+        const result = await Delete(req.body);        
+        res.send({ result })   
+})
+
+router.post('/updateQuotationItems', express.json(), async (req, res) => {
+      const result = await Update(req.body);
+    res.send({ result })
 })
 
 
