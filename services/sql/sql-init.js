4d96a188147760c98a65bc670c8d9633c7a86c8a
תכנות <@690156GUR.LOCAL>
2023-03-21 19:01:50 +0200

    add procedure deleteReceipt 21/03/2023

--- services/sql/sql-init.js
+++ services/sql/sql-init.js
@@ -445,6 +445,86 @@ async function createProcedure() {
     `);
 
     _ = await getConnection().request().query(`(
+        CREATE PROCEDURE [dbo].[pro_DeleteReceipt]
+            @ReceiptNumber NVARCHAR(30)
+        AS  
+        BEGIN
+
+            DECLARE @counter INT
+            SET @counter = 1        
+            IF(SELECT Disabled FROM BasicDetails WHERE ReceiptNumber = @ReceiptNumber)='False'
+		    BEGIN
+            WHILE (SELECT COUNT(*) AS rowNumber FROM 
+            (SELECT PaymentId AS PaymentId
+            FROM NormalizationTable
+            WHERE ReceiptNumber = @ReceiptNumber) SQ) >= @counter  
+        
+            BEGIN
+
+            IF(SELECT TOP 1 PaymentId FROM (SELECT TOP (@counter) PaymentId AS PaymentId
+            FROM NormalizationTable
+            WHERE ReceiptNumber = @ReceiptNumber
+            ORDER BY PaymentId)SQ
+            ORDER BY SQ.PaymentId DESC)  = 1
+            UPDATE Cash 
+            SET Disabled ='True',
+                DeleteDate = GETDATE()
+            WHERE ReceiptNumber = @ReceiptNumber
+        
+        
+            IF(SELECT TOP 1 PaymentId FROM (SELECT TOP (@counter) PaymentId AS PaymentId
+            FROM NormalizationTable
+            WHERE ReceiptNumber = @ReceiptNumber
+            ORDER BY PaymentId)SQ
+            ORDER BY SQ.PaymentId DESC)  = 2
+            UPDATE Cheque 
+            SET Disabled ='True',
+                DeleteDate = GETDATE()
+            WHERE ReceiptNumber = @ReceiptNumber
+             
+            IF(SELECT TOP 1 PaymentId FROM (SELECT TOP (@counter) PaymentId AS PaymentId
+            FROM NormalizationTable
+            WHERE ReceiptNumber = @ReceiptNumber
+            ORDER BY PaymentId)SQ
+            ORDER BY SQ.PaymentId DESC)  = 3
+            UPDATE Credit 
+            SET Disabled ='True',
+                DeleteDate = GETDATE()
+            WHERE ReceiptNumber = @ReceiptNumber
+             
+            IF(SELECT TOP 1 PaymentId FROM (SELECT TOP (@counter) PaymentId AS PaymentId
+            FROM NormalizationTable
+            WHERE ReceiptNumber = @ReceiptNumber
+            ORDER BY PaymentId)SQ
+            ORDER BY SQ.PaymentId DESC)  = 4
+            UPDATE StandingOrder 
+            SET Disabled ='True',
+                DeleteDate = GETDATE()
+            WHERE ReceiptNumber = @ReceiptNumber
+             
+             
+            IF(SELECT TOP 1 PaymentId FROM (SELECT TOP (@counter) PaymentId AS PaymentId
+            FROM NormalizationTable
+            WHERE ReceiptNumber = @ReceiptNumber
+            ORDER BY PaymentId)SQ
+            ORDER BY SQ.PaymentId DESC)  = 5
+            UPDATE BankTransfer 
+            SET Disabled ='True',
+                DeleteDate = GETDATE()
+            WHERE ReceiptNumber = @ReceiptNumber        
+            SET @counter = @counter+1
+            END 
+        
+            IF(SELECT Disabled FROM BasicDetails WHERE ReceiptNumber = @ReceiptNumber)='False'
+            UPDATE BasicDetails 
+            SET Disabled ='True',
+                DeleteDate = GETDATE()
+            WHERE ReceiptNumber = @ReceiptNumber
+            END
+        END
+    `) 
+
+    _ = await getConnection().request().query(`(
         CREATE PROCEDURE [dbo].[pro_DeleteCash]
             @ReceiptNumber NVARCHAR(30)
         AS  
