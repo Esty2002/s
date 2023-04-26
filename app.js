901e3fe887de7643e785cba10d336035ae865268
תכנות <@690156GUR.LOCAL>
2023-04-26 18:22:27 +0300

    build axios page to connect to the sqlServer

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 
-app.use(cors());
 const router_leads = require('./routers/leads/leads')
 const router_areas = require('./routers/price-list/areas-router')
 const router_prices = require('./routers/price-list/prices-router')
@@ -24,6 +23,7 @@ const read_router = require('./routers/receipt/read');
 const update_router = require('./routers/receipt/update');
 const delete_router = require('./routers/receipt/delete');
 
+app.use(cors());
 app.use('/leads', router_leads);
 app.use('/areas', router_areas)
 app.use('/prices', router_prices)
---
+++ services/axios.js
@@ -0,0 +1,33 @@
+require('dotenv');
+const axios=require('axios');
+const {SQL_SERVER_HOST,SQL_SERVER_PORT,}=process.env
+
+const sqlServer=axios.create({
+    baseUrl:`http://${SQL_SERVER_HOST}:${SQL_SERVER_PORT}`
+})
+
+
+const getData=async(server,url)=>{
+    let response;
+    try{
+        response=await server.get(url);
+    }
+
+    catch(error){
+        throw error;
+    }
+    return response;
+}
+
+const postData=async(server,url,body)=>{
+    let response;
+    try{
+        response=await server.post(url,body);
+    }
+
+    catch(error){
+        throw error;
+    }
+    return response;
+}
+module.exports={sqlServer,getData,postData}
