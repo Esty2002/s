09ce7bf2c84a19b8eb919ad79c7a0d1bd0b5c7e5
תכנות <@690156GUR.LOCAL>
2023-04-26 18:26:53 +0300

    complete create axios page

--- server.js
+++ server.js
@@ -4,20 +4,7 @@ const { app } = require('./app')
 
 const { HOST, PORT } = process.env;
 
-// const { connectToSql } = require('./services-clients/sql/sql-wrapper')
-const { connect } = require('./services-leads/db/mongodb/mongo-connection')
-// const { connect1 } = require('./services-price-list/db/mongo/mongo-connection')
-// const { connectToSqlSuppliers } = require('./services-suppliers/db/sql-wrapper');
-// const { connectQuotation } = require('./services-quotation/sql/sql-connection');
 
-
-// connect().then(connect1().then(connectToSql().then(connectToSqlSuppliers().then(connectQuotation().then(
-//     app.listen(PORT, HOST, () => {
-//         console.log('connect to mongo');
-//         console.log(`http://${HOST}:${PORT}`);
-//     })
-// )
-// ))))
 //            בעיות
 //               מסכים רק מספרים#
 // התאריך לא נכתב בצורה נורמלית#
@@ -25,19 +12,10 @@ const { connect } = require('./services-leads/db/mongodb/mongo-connection')
 
 
 
-// const server = http.createServer(app);
-// const { connect } = require('./services-products/db/mongo/mongo_connection')
-// const { PORT, HOST } = process.env
-
-connect().then(_ => {
-
-    console.log('connect to mongo');
-    app.listen(PORT, HOST, () => {
-        console.log(`http://${HOST}:${PORT}`);
-    })
+app.listen(PORT, HOST, () => {
+    console.log(`http://${HOST}:${PORT}`);
 })
-// sconnect()
-const { createTables,createProcedure } = require('./services-receipt/sql/sql-init');
+
 
 
 
