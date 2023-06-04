3ecf9c8e9530c9499393c6bc840ea0cbf107840c
Rut <@690156GUR.LOCAL>
2023-06-04 14:19:26 +0300

    finish the server

--- modules/products/finishProducts.js
+++ modules/products/finishProducts.js
@@ -8,7 +8,7 @@ async function insertFinishProduct(obj) {
     obj['unitOfMeasure'] = (await findMeasureNumber(obj['unitOfMeasure'])).data[0].id
     // obj['ordinalNumber'] = await (getData(sqlServer, '/')) + 1
     obj['addedDate'] = new Date().toISOString().slice(0, new Date().toISOString().indexOf('T'))
-    return await postData(sqlServer, '/create/create', { tableName: SQL_FINISH_PRODUCTS_TABLE, values: obj })
+    return (await postData(sqlServer, '/create/create', { tableName: SQL_FINISH_PRODUCTS_TABLE, values: obj })).data
 }
 
 async function updateFinishProduct(data = {}, condition = {}) {
@@ -17,7 +17,7 @@ async function updateFinishProduct(data = {}, condition = {}) {
         string += `${k}='${data[k]}',`
     }
     string = string.slice(0, -1)
-    return await postData(sqlServer, 'update/update', { tableName: SQL_FINISH_PRODUCTS_TABLE, values: data, condition: condition ? `${Object.keys(condition)[0]}='${Object.values(condition)[0]}'` : "" })
+    return (await postData(sqlServer, 'update/update', { tableName: SQL_FINISH_PRODUCTS_TABLE, values: data, condition: condition ? `${Object.keys(condition)[0]}='${Object.values(condition)[0]}'` : "" })).data
 }
 
 async function findFinishProduct(project = [], filter = {}) {
