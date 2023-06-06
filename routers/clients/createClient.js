ac44d7f1b868aa109ea2021a12be35b5192ebe20
stsparctice <practicestuds@gmail.com>
2023-06-06 04:06:33 +0300

    clients and suppliers

--- routers/clients/createClient.js
+++ routers/clients/createClient.js
@@ -3,7 +3,6 @@ const router = express.Router()
 const { addOneClient } = require('../../modules/clients/createClient')
 
 router.post('/add', express.json(), async (req, res) => {
-    console.log(req.body)
     const result = await addOneClient(req.body);
     if (result == false)
         res.status(200).send(false)
