6071153ff993411c7585065272f88e328478601a
תכנות <@690156GUR.LOCAL>
2023-06-20 18:02:02 +0300

    utils

--- routers/utils/auto_complete.js
+++ routers/utils/auto_complete.js
@@ -7,6 +7,7 @@ router.get('/autocomplete/:table/:column/:word', async (req, res) => {
     try {
         console.log(req.params, "tableName");
         const result = await findAuto(req.params.table, req.params.column, req.params.word);
+        console.log(result,"result");
         res.status(200).send(result);
     }
     catch (error) {
