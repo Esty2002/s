4bfb03ae3fb7b636bf5cdbc51611a974eff08446
stsparctice <practicestuds@gmail.com>
2023-06-22 09:32:20 +0300

    delete files

--- routers/products/unit_of_measure.js
+++ routers/products/unit_of_measure.js
@@ -5,7 +5,7 @@ const { findMeasureName, findMeasureNumber, insertMeasure, updateMeasure, getAll
 
 router.get('/findMeasureName', async (req, res) => {
     try {
-        const response =await findMeasureName(req.query.id)
+        const response = await findMeasureName(req.query.id)
         if (response)
             res.status(200).send(response)
         else {
@@ -18,7 +18,7 @@ router.get('/findMeasureName', async (req, res) => {
 
 router.get('/findMeasureId', async (req, res) => {
     try {
-        const response =await findMeasureNumber(req.query.name)
+        const response = await findMeasureNumber(req.query.name)
         if (response)
             res.status(200).send(`${response}`)
         else {
@@ -31,13 +31,13 @@ router.get('/findMeasureId', async (req, res) => {
 
 router.post('/create', express.json(), async (req, res) => {
     try {
-        const response =await insertMeasure(req.body.new)
+        const response = await insertMeasure(req.body.new)
 
-        console.log({response:response})
-        if (response.data.Id)
-            res.status(201).send(response.data.Id)
+        console.log({ response: response })
+        if (response.status === 201)
+            res.status(201).send(response.data)
         else {
-            res.status(500).send(-1)
+            res.status(response.status).send(-1)
         }
     } catch (error) {
         res.status(500).send(error.message)
@@ -46,7 +46,7 @@ router.post('/create', express.json(), async (req, res) => {
 
 router.post('/update', express.json(), async (req, res) => {
     try {
-        const response =await updateMeasure(req.body.prev, req.body.new)
+        const response = await updateMeasure(req.body.prev, req.body.new)
         if (response)
             res.status(200).send(response)
         else {
@@ -59,7 +59,7 @@ router.post('/update', express.json(), async (req, res) => {
 
 router.get('/all', async (req, res) => {
     try {
-        const response =await getAll()
+        const response = await getAll()
         console.log(response);
         if (response)
             res.status(200).send(response)
