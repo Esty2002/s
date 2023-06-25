ff4522153e82230a3917a0a3ead37d829fd8c027
תכנות <@690156GUR.LOCAL>
2023-06-25 18:33:49 +0300

    changes after pull

--- app.js
+++ app.js
@@ -13,17 +13,13 @@ const manageFinishProducts = require('./routers/products/finishProducts');
 const branches_router = require('./routers/suppliers/branches');
 const suppliers_router = require('./routers/suppliers/suppliers');
 const delete_client_router = require('./routers/clients/deleteClients');
-const createClient_router = require('./routers/clients/createClient')
-const readClient_router = require('./routers/clients/readClient')
 const updateClient_router = require('./routers/clients/updateClient')
 const status_router = require('./routers/clients/status')
 const auto_complete=require('./routers/utils/auto_complete')
 const productsCombinations_router = require('./routers/products/productsCombinations')
 const pricelist_router=require('./routers/price-list/pricelist')
-const areas_router = require('./routers/areas/areas');
 const {  reqLogger } = require('./services/logger/logger');
 
-const areas_router = require('./routers/areas/areas');
 const api_router = require('./api/routers/readFile');
 const areas_router = require('./routers/areas/areas')
 
@@ -54,12 +50,10 @@ app.use('/updateClient', updateClient_router)
 
 app.use(cors());
 // app.use('/leads', router_leads);
-app.use('/areas', areas_router)
 app.use('/pricesNew', pricelist_router)
 app.use('/updateClient',updateClient_router)
 app.use('/createClient', createClient_router)
 app.use('/delete_client', delete_client_router);
-app.use('/readClient', readClient_router);
 app.use('/statusesClient', status_router);
 app.use('/auto_complete', auto_complete);
 
@@ -82,7 +76,7 @@ app.use('/productsCombinations', productsCombinations_router);
 app.get('/', (req, res) => {
     res.status(200).send('hello buyton');
 });
-rmSync
+
 app.get('/*', (req, res) => {
     res.status(200).send('request not found');
 });
