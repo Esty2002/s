ff2073f6970082b20d6e3409cacf3e7ddaad8485
תכנות <@690156GUR.LOCAL>
2023-07-04 12:57:41 +0300

    changes in areas'

--- routers/areas/areas.js
+++ routers/areas/areas.js
@@ -74,16 +74,6 @@ router.post('/dropArea', express.json(), async (req, res) => {
 
 })
 
-// router.get('/findAllAreas', async (req, res) => {
-//     try {
-//         const result = await findAll()
-//         console.log('-------',result);
-//         res.status(200).send(result)
-//     }
-//     catch (err) {
-//         res.status(500).send(err)
-//     }
-// })
 
 router.get('/findAll/:filter', async (req, res) => {
     let filter = req.params.filter
@@ -152,7 +142,6 @@ router.post('/updateArea', express.json(), async (req, res) => {
         res.status(500).send(error.message)
     }
 })
-// giti
 router.post('/updatePointsInArea', express.json(), async (req, res) => {
     try {
         const response = await updateLocation(req.body)
@@ -166,7 +155,6 @@ router.post('/updatePointsInArea', express.json(), async (req, res) => {
     }
 })
 
-// giti
 router.post('/updatePointsAndRadiusInArea', express.json(), async (req, res) => {
     try {
         const response = await updatePointAndRadius(req.body)
@@ -180,52 +168,6 @@ router.post('/updatePointsAndRadiusInArea', express.json(), async (req, res) => 
     }
 })
 
-// o.k
-router.post('/deleteAreaDetail', express.json(), async (req, res) => {
-    //req.body צריך לקבל מס' {טלפון,שם אזור} ב
-    let phone = req.body.phone
-    let nameArea = req.body.areaName
-
-    try {
-        const response = await deleteArea(phone, nameArea)
-        if (response)
-            res.status(200).send(response)
-        else {
-            res.status(500).send(response)
-        }
-    } catch (error) {
-        res.status(500).send(error.message)
-    }
-})
-
-// o.k
-
-
-
-
-// למה צריך את זה - זה כמו הבא אחריו
-// router.get('/findAllAreas/:code', async (req, res) => {
-//     try {
-//         const result = await findAreaByCode(req.params.code)
-//         res.status(200).send(result)
-//     } catch (error) { 
-//         res.status(404).send(error)
-//     }
-
-// })
-
-router.get('/findAreasByCode/:code', async (req, res) => {
-    try {
-        const response = await findAreaByCode(req.params.code)
-        if (response)
-            res.status(200).send(response)
-        else {
-            res.status(500).send(response)
-        }
-    } catch (error) {
-        res.status(500).send(error.message)
-    }
-})
 
 //giti...
 router.get('/findPointArray/:code/:areaName', async (req, res) => {
@@ -241,13 +183,6 @@ router.get('/findPointArray/:code/:areaName', async (req, res) => {
     }
 })
 
-
-// //מקבל קוד ספק/לקוח וכן שם אזור
-// // בודק האם אזור זה קיים 
-// router.get('/findAreaOfSupplierOrClient', async (req, res) => {
-//     const result = await findAreaOfSupplierOrClient(req.query.code, req.query.areaName)
-//     res.status(200).send(result)
-// })
 
 
 
