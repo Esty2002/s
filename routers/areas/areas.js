18f0bb47adc99291874826e208fe30a28e21e9d4
תכנות <@690156GUR.LOCAL>
2023-07-19 18:39:29 +0300

    start tests

--- routers/areas/areas.js
+++ routers/areas/areas.js
@@ -3,13 +3,13 @@ const { cache } = require('ejs')
 const express = require('express');
 const { object } = require('webidl-conversions');
 
-const { insertArea,startt, updateArea, deleteArea, findAreas, findByDistinct, getFromSql, getFromMongo, findInPolygon,findInRadius } = require('../../modules/areas/areas');
+const { insertArea, startt, updateArea, deleteArea, findAreas, findByDistinct, getFromSql, getFromMongo, findInPolygon, findInRadius } = require('../../modules/areas/areas');
 const { logToFile } = require('../../services/loggerPnini');
 
-router.get('/', async (req, res) => {
-    const r = await startt()
-    res.send(r.data)
-});
+// router.get('/', async (req, res) => {
+//     const r = await startt()
+//     res.send(r.data)
+// });
 
 router.post('/insertArea', express.json(), async (req, res) => {
     let object;
@@ -21,168 +21,169 @@ router.post('/insertArea', express.json(), async (req, res) => {
         }
         logToFile(object)
         const result = await insertArea(req.body);
-        if (result) {
+        if (result.status == 201) {
             res.status(201).send(result);
         }
         else {
-            res.status(500).send();
+            console.log('rrrrrrrrrrrrrrrrs', result);
+            res.status(result.status).send(result.data);
         }
     }
     catch (error) {
         object.error = error.message
         logToFile(object)
-        res.status(500).send(error);
+        res.status(500).send(error.message);
     }
 });
 
-router.post('/findAreas', express.json(), async (req, res) => {
-    let object;
-    try {
-        object = {
-            name: 'findAreas',
-            description: 'findAreas in router- in try',
-            dataThatRecived: req.body
-        }
-        logToFile(object)
-        const result = await findAreas(req.body);
-        console.log('***********************', result);
-        res.status(200).send(result);
-    }
-    catch (err) {
-        object.error = err.message
-        logToFile(object)
-        res.status(500).send(err);
-    }
-});
+// router.post('/findAreas', express.json(), async (req, res) => {
+//     let object;
+//     try {
+//         object = {
+//             name: 'findAreas',
+//             description: 'findAreas in router- in try',
+//             dataThatRecived: req.body
+//         }
+//         logToFile(object)
+//         const result = await findAreas(req.body);
+//         console.log('***********************', result);
+//         res.status(200).send(result);
+//     }
+//     catch (err) {
+//         object.error = err.message
+//         logToFile(object)
+//         res.status(500).send(err);
+//     }
+// });
 
-//temporary router
-router.get('/readFromSql', async (req, res) => {
-    try {
-        const result = await getFromSql();
-        res.status(200).send(JSON.stringify(result));
-    }
-    catch (err) {
-        res.status(404).send(err);
-    }
-});
+// //temporary router
+// router.get('/readFromSql', async (req, res) => {
+//     try {
+//         const result = await getFromSql();
+//         res.status(200).send(JSON.stringify(result));
+//     }
+//     catch (err) {
+//         res.status(404).send(err);
+//     }
+// });
 
-//temporary router
-router.get('/readFromMongo', async (req, res) => {
-    try {
-        const result = await getFromMongo();
-        res.status(200).send(JSON.stringify(result));
-    }
-    catch (err) {
-        res.status(404).send(err);
-    }
-});
+// //temporary router
+// router.get('/readFromMongo', async (req, res) => {
+//     try {
+//         const result = await getFromMongo();
+//         res.status(200).send(JSON.stringify(result));
+//     }
+//     catch (err) {
+//         res.status(404).send(err);
+//     }
+// });
 
-router.post('/searchAreas', express.json(), async (req, res) => {
-    let obj = req.body
+// router.post('/searchAreas', express.json(), async (req, res) => {
+//     let obj = req.body
 
-    let object;
-    try {
-        object = {
-            name: 'searchAreas',
-            description: 'searchAreas in router- in try',
-            dataThatRecived: req.body
-        }
-        logToFile(object)
-        let areas = [];
-        const citys = await findAreas({ basicName: obj.city });
-        const points = await findAreas({ point: obj.point, type: 'point' });
-        // const radius = await findAreas({ type: 'radius' });
-        const radius = await findInRadius({ point: obj.point,type: 'radius' });
-        // //--------------------------------------
-        // db.getCollection('tA').aggregate([
-        //     {
-        //       $geoNear: {
-        //         near: {
-        //           type: "Point",
-        //           coordinates: [31.2712026, 35.2130481] 
-        //         },
-        //         distanceField: "calculatedDist",
-        //         maxDistance: 4500,
-        //       spherical: true
-        //       }
-        //     },
-        //     {$match: {$expr: {$gte:['$radius', '$calculatedDist']},filter}}
-        //   ])
-// ---------------
-        // key: 'gpsLocation',
+//     let object;
+//     try {
+//         object = {
+//             name: 'searchAreas',
+//             description: 'searchAreas in router- in try',
+//             dataThatRecived: req.body
+//         }
+//         logToFile(object)
+//         let areas = [];
+//         const citys = await findAreas({ basicName: obj.city });
+//         const points = await findAreas({ point: obj.point, type: 'point' });
+//         // const radius = await findAreas({ type: 'radius' });
+//         const radius = await findInRadius({ point: obj.point, type: 'radius' });
+//         // //--------------------------------------
+//         // db.getCollection('tA').aggregate([
+//         //     {
+//         //       $geoNear: {
+//         //         near: {
+//         //           type: "Point",
+//         //           coordinates: [31.2712026, 35.2130481] 
+//         //         },
+//         //         distanceField: "calculatedDist",
+//         //         maxDistance: 4500,
+//         //       spherical: true
+//         //       }
+//         //     },
+//         //     {$match: {$expr: {$gte:['$radius', '$calculatedDist']},filter}}
+//         //   ])
+//         // ---------------
+//         // key: 'gpsLocation',
 
-// -----------------
-        //   {
-        //       "_id" : ObjectId("626ef666653cf1987b9fefba"),
-        //       "cirleName" : "test",
-        //       "radius" : 5500000, // meters
-        //       "gpsLocation" : {
-        //           "type" : "Point",
-        //           "coordinates" : [0, 0] // circle center point geoJson coordinates
-        //       }
-        //   },
-        //   {
-        //       "_id" : ObjectId("626ef695653cf1987b9feff3"),
-        //       "cirleName" : "test1",
-        //       "radius" : 100000, // meters
-        //       "gpsLocation" : {
-        //           "type" : "Point",
-        //           "coordinates" : [0, 80] // circle center point geoJson coordinates
-        //       }
-        //   },
-        //   {
-        //       "_id" : ObjectId("626ef6a9653cf1987b9ff001"),
-        //       "cirleName" : "test2",
-        //       "radius" : 7000000, // meters
-        //       "gpsLocation" : {
-        //           "type" : "Point",
-        //           "coordinates" : [40, 40] // circle center point geoJson coordinates
-        //       }
-        //   }
+//         // -----------------
+//         //   {
+//         //       "_id" : ObjectId("626ef666653cf1987b9fefba"),
+//         //       "cirleName" : "test",
+//         //       "radius" : 5500000, // meters
+//         //       "gpsLocation" : {
+//         //           "type" : "Point",
+//         //           "coordinates" : [0, 0] // circle center point geoJson coordinates
+//         //       }
+//         //   },
+//         //   {
+//         //       "_id" : ObjectId("626ef695653cf1987b9feff3"),
+//         //       "cirleName" : "test1",
+//         //       "radius" : 100000, // meters
+//         //       "gpsLocation" : {
+//         //           "type" : "Point",
+//         //           "coordinates" : [0, 80] // circle center point geoJson coordinates
+//         //       }
+//         //   },
+//         //   {
+//         //       "_id" : ObjectId("626ef6a9653cf1987b9ff001"),
+//         //       "cirleName" : "test2",
+//         //       "radius" : 7000000, // meters
+//         //       "gpsLocation" : {
+//         //           "type" : "Point",
+//         //           "coordinates" : [40, 40] // circle center point geoJson coordinates
+//         //       }
+//         //   }
 
-        // db.places.find( {
-        //     loc: { $geoWithin: { $centerSphere: [ [ -88, 30 ], 10/3963.2 ] } }
-        //   } )
+//         // db.places.find( {
+//         //     loc: { $geoWithin: { $centerSphere: [ [ -88, 30 ], 10/3963.2 ] } }
+//         //   } )
 
-        // db.restaurants.find({ location:
-        //     { $geoWithin:
-        //        { $centerSphere: [ [ -73.93414657, 40.82302903 ], 5 / 6378.1 ] } } })
+//         // db.restaurants.find({ location:
+//         //     { $geoWithin:
+//         //        { $centerSphere: [ [ -73.93414657, 40.82302903 ], 5 / 6378.1 ] } } })
 
-        // db.places.find(
-        //     { loc: { $geoWithin: { $center: [ [-74, 40.74], 10 ] } } }
-        //  )
-        //--------------------------------------
+//         // db.places.find(
+//         //     { loc: { $geoWithin: { $center: [ [-74, 40.74], 10 ] } } }
+//         //  )
+//         //--------------------------------------
 
-        const polygon = await findInPolygon({ point: obj.point });
-        areas = [...areas, ...citys, ...points, ...radius, ...polygon];
-        console.log('popopopo',radius);
-        res.status(200).send(areas)
-    }
-    catch (error) {
-        object.error = error.message
-        logToFile(object)
-        res.status(500).send(error)
-    }
-});
+//         const polygon = await findInPolygon({ point: obj.point });
+//         areas = [...areas, ...citys, ...points, ...radius, ...polygon];
+//         console.log('popopopo', radius);
+//         res.status(200).send(areas)
+//     }
+//     catch (error) {
+//         object.error = error.message
+//         logToFile(object)
+//         res.status(500).send(error)
+//     }
+// });
 
-router.post('/findAllTypes', express.json(), async (req, res) => {
-    let object;
-    try {
-        object = {
-            name: 'findAllTypes',
-            description: 'findAllTypes in router- in try',
-            dataThatRecived: req.body
-        }
-        logToFile(object)
-        const result = await findByDistinct(req.body)
-        res.status(200).send(result.data)
-    }
-    catch (err) {
-        object.error = err.message
-        logToFile(object)
-        res.status(500).send(err)
-    }
-});
+// router.post('/findAllTypes', express.json(), async (req, res) => {
+//     let object;
+//     try {
+//         object = {
+//             name: 'findAllTypes',
+//             description: 'findAllTypes in router- in try',
+//             dataThatRecived: req.body
+//         }
+//         logToFile(object)
+//         const result = await findByDistinct(req.body)
+//         res.status(200).send(result.data)
+//     }
+//     catch (err) {
+//         object.error = err.message
+//         logToFile(object)
+//         res.status(500).send(err)
+//     }
+// });
 
 router.post('/updateArea', express.json(), async (req, res) => {
     let object;
@@ -194,10 +195,10 @@ router.post('/updateArea', express.json(), async (req, res) => {
         }
         logToFile(object)
         const response = await updateArea(req.body);
-        if (response)
-            res.status(200).send(response);
+        if (response.status == 204)
+            res.status(204).send(response);
         else {
-            res.status(500).send(response);
+            res.status(response.status).send(response.data);
         }
 
     } catch (error) {
@@ -206,27 +207,27 @@ router.post('/updateArea', express.json(), async (req, res) => {
         res.status(500).send(error.message);
     }
 });
-router.post('/deleteArea', express.json(), async (req, res) => {
-    let object;
-    try {
-        object = {
-            name: 'deleteArea',
-            description: 'deleteArea in router- in try',
-            dataThatRecived: req.body
-        }
-        logToFile(object);
-        const response = await deleteArea(req.body);
-        if (response)
-            res.status(200).send(response);
-        else {
-            res.status(500).send(response);
-        }
+// router.post('/deleteArea', express.json(), async (req, res) => {
+//     let object;
+//     try {
+//         object = {
+//             name: 'deleteArea',
+//             description: 'deleteArea in router- in try',
+//             dataThatRecived: req.body
+//         }
+//         logToFile(object);
+//         const response = await deleteArea(req.body);
+//         if (response)
+//             res.status(200).send(response);
+//         else {
+//             res.status(500).send(response);
+//         }
 
-    } catch (error) {
-        object.error = error.message
-        logToFile(object)
-        res.status(500).send(error.message);
-    }
-})
+//     } catch (error) {
+//         object.error = error.message
+//         logToFile(object)
+//         res.status(500).send(error.message);
+//     }
+// })
 
 module.exports = router;
